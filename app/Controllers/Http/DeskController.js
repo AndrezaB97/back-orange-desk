7f13da0eb5bc02b9a55577f8eeb02e541ac26eef
AndrezaB97 <andreza.adsb@gmail.com>
2021-09-15 14:53:47 -0300

    fix/desks

--- app/Controllers/Http/DeskController.js
+++ app/Controllers/Http/DeskController.js
@@ -24,6 +24,7 @@ class DeskController {
       .where("date", "=", data.date)
       .count();
 
+    return numDesk.count;
     let unity = await Unity.find(data.unity_id);
 
     let result = (await unity.capacity_allowed) - numDesk.count;
